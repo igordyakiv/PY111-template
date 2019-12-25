7d24a4be4e39c230e983e4e320446ab95a6882dc
Дякив Игорь Богданович <IGODYA@avalon.ru>
2019-12-25 20:51:48 +0300

    d0 done

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,5 +8,10 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
 	:param stairway: list of ints, where each int is a cost of appropriate step
 	:return: minimal cost of getting to the top
 	"""
-	print(stairway)
-	return 0
+	cost = [0 for i in stairway]  # заполни масив нулями такого же размера
+	cost[0] = stairway[0]
+	cost[1] = stairway[1]
+	for i in range(2, len(stairway)):
+		cost[i] = min(cost[i - 1], cost[i - 2]) + stairway[i]
+
+	return cost[-1]
